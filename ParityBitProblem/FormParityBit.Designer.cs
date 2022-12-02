238e6cfad8a3178822b9a45ed490c580cf7a754e
Kiril Spasov <spasov.kiril@gmail.com>
2022-12-02 12:23:43 +0200

    Add project files.

---
+++ ParityBitProblem/FormParityBit.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace ParityBitProblem
+{
+    partial class FormParityBit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnCheck = new System.Windows.Forms.Button();
+            this.TxtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtnCheck
+            // 
+            this.BtnCheck.Location = new System.Drawing.Point(33, 46);
+            this.BtnCheck.Name = "BtnCheck";
+            this.BtnCheck.Size = new System.Drawing.Size(178, 60);
+            this.BtnCheck.TabIndex = 0;
+            this.BtnCheck.Text = "Check Parity Bit";
+            this.BtnCheck.UseVisualStyleBackColor = true;
+            this.BtnCheck.Click += new System.EventHandler(this.BtnCheck_Click);
+            // 
+            // TxtResult
+            // 
+            this.TxtResult.Location = new System.Drawing.Point(263, 46);
+            this.TxtResult.Multiline = true;
+            this.TxtResult.Name = "TxtResult";
+            this.TxtResult.Size = new System.Drawing.Size(486, 362);
+            this.TxtResult.TabIndex = 1;
+            // 
+            // FormParityBit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TxtResult);
+            this.Controls.Add(this.BtnCheck);
+            this.Name = "FormParityBit";
+            this.Text = "Parity Bit Problem";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnCheck;
+        private System.Windows.Forms.TextBox TxtResult;
+    }
+}
+
---
+++ ParityBitProblem/FormParityBit.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ParityBitProblem
+{
+    public partial class FormParityBit : Form
+    {
+        public FormParityBit()
+        {
+            InitializeComponent();
+        }
+
+        int countOnes;
+        string binary;
+
+        private void BtnCheck_Click(object sender, EventArgs e)
+        {
+            string line = "";
+
+            string path = Application.StartupPath + @"\parity.txt";
+            StreamReader streamReader = new StreamReader(path);
+
+            bool finished = false;
+
+            while (!finished)
+            {
+                line = streamReader.ReadLine();
+
+                if (line == null)
+                {
+                    finished = true;
+                }
+                else
+                {
+                    CheckParity(FindBinaryBits(Convert.ToInt32(line)));
+
+                    if (countOnes % 2 == 0)
+                    {
+                        TxtResult.Text += line + " - " + binary + " - 0" + Environment.NewLine;
+                    }
+                    else
+                    {
+                        TxtResult.Text += line + " - " + binary + " - 1" + Environment.NewLine;
+                    }
+                }
+            }
+        }
+
+        private int[] FindBinaryBits(int number)
+        {
+            int[] bits = new int[] { 0, 1, 2, 4, 8, 16, 32, 64 };
+            int[] bitsCount = new int[65];
+
+            binary = "";
+
+            //Per assignment input is 0 <= N <= 128.
+            //First we divide the number by 64, if result is >= 1,
+            //we add 1 in the array counter to the corresponding bit.
+            //We do this for all 7 bits.
+            for (int i = bits.Length - 1; i >= 1; i--)
+            {
+                if (number / bits[i] >= 1)
+                {
+                    bitsCount[bits[i]] += 1;
+                    number = number - bits[i];
+                    binary += "1";
+                }
+                else
+                {
+                    binary += "0";
+                }
+            }
+            return bitsCount;
+        }
+
+        private void CheckParity(int[] bitsCount)
+        {
+            countOnes = 0;
+
+            for (int i = 0; i < bitsCount.Length; i++)
+            {
+                if (bitsCount[i] == 1)
+                    countOnes++;
+            }
+        }
+    }
+}
